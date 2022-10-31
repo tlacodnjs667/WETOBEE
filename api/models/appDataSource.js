6b11fc70b8fabfed18d7039e7951b44458bba335
tlacodnjs667 <tlacodnjs667@gmail.com>
2022-10-31 19:51:25 +0900

    초기 세팅

---
+++ api/models/appDataSource.js
@@ -0,0 +1,22 @@
+const { DataSource } = require('typeorm');
+
+const appDataSource = new DataSource({
+    type:process.env.TYPEORM_CONNECTION,
+    host:process.env.TYPEORM_HOST,
+    port:process.env.TYPEORM.PORT,
+    password:process.env.TYPEORM_PASSWORD,
+    username:process.env.TYPEORM_USERNAME,
+    database:process.env.TYPEORM_DATABASE
+})
+
+appDataSource.initialize()
+    .then(()=>{
+        console.log('Data Source has been initialized!');
+    })
+    .catch((err)=>{
+        console.error('Error during Data Source initialization')
+        appDataSource.destroy();
+    })
+
+
+module.exports={appDataSource};
