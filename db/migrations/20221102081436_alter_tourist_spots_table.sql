0a147113d45a09968ca33a7152f907015b89fc0d
tlacodnjs667 <tlacodnjs667@gmail.com>
2022-11-03 12:58:36 +0900

    투어리스트 스팝 테이블의  위, 경도 데이타 삭제하고 모든 데이터 포함되어 있는  JSON 형식으로 변경

--- db/migrations/20221102081436_alter_tourist_spots_table.sql
+++
@@ -1,8 +0,0 @@
--- migrate:up
-ALTER TABLE tourist_spots 
-    ADD longtitude DECIMAL(18, 10) NOT NULL,
-    ADD latitude DECIMAL(18, 6) NOT NULL;
--- migrate:down
-ALTER TABLE tourist_spots 
-    DROP longtitude, 
-    DROP latitude;
